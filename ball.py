10d767d4f64552d0d4083bb2131bdff8a97bddba
HasinMahir <168253827+HasinMahir@users.noreply.github.com>
2024-05-07 08:45:08 -0400

    Add files via upload
    I researched how to use the ellipse function in Python. I started working on my Ball class and rescaled an image. My code will draw the ball on the screen after the user clicks off the title screen.

---
+++ ball.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Ball:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("ball.jpg")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 0.35
+
+    def move(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        elif direction == "left":
+            self.x = self.x - self.delta
+        elif direction == "up":
+            self.y = self.y - self.delta
+        elif direction == "down":
+            self.y = self.y + self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
+from ball import Ball
 
 # set up pygame modules
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 30)
 title_font = pygame.font.SysFont("Arial", 100)
-pygame.display.set_caption("Ping Pong")
+pygame.display.set_caption("Pong")
 
-# set up variables for the display
+# set up variables
 size = (900, 650)
 screen = pygame.display.set_mode(size)
-
-# render the text for later
+ball = Ball(50, 50)
 
 # the loop will carry on until the user exits the game
 run = True
 game_over = False
 display = False
+counter = 0
 
 # -------- Main Program Loop -----------
 while run:
@@ -24,13 +25,18 @@ while run:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
+        # start the game by clicking off the title screen
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1 and counter == 0:
+                display = True
+                counter = counter + 1
 
     screen.fill((0, 0, 0))
 
     # title screen
     if not display and not game_over:
-        instructions_1 = "Ping"
-        instructions_1_cont = "Pong"
+        instructions_1 = "Po"
+        instructions_1_cont = "ng"
         instructions_2 = "Use the W and S keys for Player 1"
         instructions_3 = "Use the up and down arrow keys for Player 2"
         instructions_4 = "First to 7 points wins!"
@@ -41,12 +47,17 @@ while run:
         display_instruction_3 = my_font.render(instructions_3, True, (255, 255, 255))
         display_instruction_4 = my_font.render(instructions_4, True, (255, 255, 255))
         display_instruction_5 = my_font.render(instructions_5, True, (255, 255, 255))
-        screen.blit(display_instruction_1, (250, 0))
+        screen.blit(display_instruction_1, (335, 0))
         screen.blit(display_instruction_1_cont, (435, 0))
         screen.blit(display_instruction_2, (250, 200))
         screen.blit(display_instruction_3, (250, 250))
         screen.blit(display_instruction_4, (250, 300))
         screen.blit(display_instruction_5, (250, 350))
+
+    # when game is running
+    if display and not game_over:
+        screen.blit(ball.image, ball.rect)
+
 
     pygame.display.update()
 

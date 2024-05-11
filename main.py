8ddb0d5e848004e206c649005cd46f987fa69486
HasinMahir <168253827+HasinMahir@users.noreply.github.com>
2024-05-10 22:06:01 -0400

    Add files via upload
    Tonight, I fixed my ball animation. I also created my player class and created two players. The ball will bounce if it collides with the players.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from ball import Ball
+from player import Player
 
 # set up pygame modules
 pygame.init()
@@ -11,8 +12,13 @@ pygame.display.set_caption("Pong")
 # set up variables
 size = (900, 650)
 screen = pygame.display.set_mode(size)
-# (870, 620)
 ball = Ball(430, 300)
+player1 = Player(0, 250)
+player2 = Player(889, 250)
+x_delta = 0.3
+y_delta = 0.3
+player1_score = 0
+player2_score = 0
 
 # the loop will carry on until the user exits the game
 run = True
@@ -59,17 +65,44 @@ while run:
     if display and not game_over:
         pygame.draw.ellipse(screen, (255, 255, 255), ball.rect)
         # move the ball by adding the delta to its x and y position
-        x_delta = 0.3
-        y_delta = 0.3
-        # ball.x = ball.x + x_delta
+        ball.x = ball.x + x_delta
         ball.y = ball.y + y_delta
         ball.move()
+        screen.blit(player1.image, player1.rect)
+        screen.blit(player2.image, player2.rect)
 
         # if the ball hits the wall, make it bounce by inverting the x and y deltas
-        # if ball.x >= 870:
-        #     x_delta = x_delta * -1
-        if ball.y >= 620:
+        if ball.x >= 870:
+            x_delta = x_delta * -1
+            # set the ball back to the middle of the screen
+            ball.x = 430
+            ball.y = 300
+            player1_score = player1_score + 1
+
+        if ball.x <= 0:
+            x_delta = x_delta * -1
+            # set the ball back to the middle of the screen
+            ball.x = 430
+            ball.y = 300
+            player2_score = player2_score + 1
+
+        if ball.y >= 620 or ball.y <= 0:
             y_delta = y_delta * -1
+
+        # player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player1.move_direction("player 1 up")
+        if keys[pygame.K_s]:
+            player1.move_direction("player 1 down")
+        if keys[pygame.K_UP]:
+            player2.move_direction("player 2 up")
+        if keys[pygame.K_DOWN]:
+            player2.move_direction("player 2 down")
+
+        # if the ball collides with the player, make it bounce
+        if ball.rect.colliderect(player1.rect) or ball.rect.colliderect(player2.rect):
+            x_delta = x_delta * -1
 
     pygame.display.update()
 
---
+++ player.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Player:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("player.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def move_direction(self, direction):
+        if direction == "player 1 up" and not self.y <= 0:
+            self.y = self.y - 0.5
+        if direction == "player 1 down" and not self.y >= 560:
+            self.y = self.y + 0.5
+        if direction == "player 2 up" and not self.y <= 0:
+            self.y = self.y - 0.5
+        if direction == "player 2 down" and not self.y >= 560:
+            self.y = self.y + 0.5
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
